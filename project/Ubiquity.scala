6602299b931eb4ca6a810974c8f657b29d46ef87
George Adams <g.w.adams.iv@gmail.com>
2011-09-28 22:59:45 -0400

    well, i got a build to work. i don't like that i had to make a root project, but i'll deal with that later. i might have to talk to my local scala buddies about that.

--- project/Ubiquity.scala
+++ project/Ubiquity.scala
@@ -1,7 +1,8 @@
 import sbt._
 
 object Ubiquity extends Build {
-  lazy val common: Project = Project("Common", file("Common"))
+  lazy val root: Project = Project("root", file(".")) aggregate(common, client, server)
+  lazy val common: Project = Project("Common", file("Common")) 
   lazy val client: Project = Project("Client", file("Client")) dependsOn(common)
   lazy val server: Project = Project("Server", file("Server")) dependsOn(common)
 }
