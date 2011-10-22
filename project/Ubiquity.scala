e05ce37069192fb968a7feee0309edf5fcba3cc8
AlbertoPL <apareja@albertopl.com>
2011-10-22 19:45:06 -0400

    Minor changes to Ubiquity.scala

--- project/Ubiquity.scala
+++ project/Ubiquity.scala
@@ -1,8 +1,9 @@
 import sbt._
 
 object Ubiquity extends Build {
-  lazy val root: Project = Project("root", file(".")) aggregate(common, client, server)
+  lazy val root: Project = Project("root", file(".")) aggregate(common, client, server, web)
   lazy val common: Project = Project("Common", file("Common")) 
   lazy val client: Project = Project("Client", file("Client")) dependsOn(common)
   lazy val server: Project = Project("Server", file("Server")) dependsOn(common)
+  lazy val web: Project = Project("WebUbiquity", file("WebUbiquity")) dependsOn(common, server)
 }
